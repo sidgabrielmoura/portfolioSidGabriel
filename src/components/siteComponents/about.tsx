88ad16d15a5efe6bb81196f40dc0731f23696492
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2023-12-20 12:20:52 -0300

    about page responsivity finished

--- src/components/siteComponents/about.tsx
+++ src/components/siteComponents/about.tsx
@@ -3,22 +3,32 @@ import { IconsAbout } from "./iconsAboutPage";
 export function About(){
     return(
         <>
-            <main className="flex gap-3 items-center justify-center h-[80vh]
-            lg:px-[50px] lg:py-[25px]
-            min-[320px]:px-[25px] min-[320px]:py-[20px]">
+            <main className="flex items-center justify-center h-[80vh]
+            lg:px-[50px] lg:py-[25px] lg:flex-row lg:gap-3
+            min-[320px]:px-[25px] min-[320px]:py-[20px] min-[320px]:flex-col min-[320px]:justify-around min-[320px]:gap-5">
 
-                <section className="w-[15vw] h-full flex flex-col border-r-[1.5px] border-zinc-400 justify-center">
+                <section className="flex justify-center
+                lg:h-full lg:flex-col lg:border-r-[1.5px] lg:border-zinc-400 lg:w-[15vw]
+                min-[320px]:flex-row min-[320px]:w-full">
                     
                     <IconsAbout/>
 
                 </section>
 
-                <section className="w-full h-full flex justify-center items-center gap-5 border-dashed border border-zinc-400 rounded-lg">
-                    <div className="w-[15vw] brightness-75 h-[30vh] rounded-full bg-[url(../assets/sidImage.jpeg)] bg-cover bg-center bg-no-repeat
-                    outline outline-offset-4 outline-zinc-400"/>
-                    <p className="w-[40vw] text-zinc-300 p-[20px] rounded-lg bg-zinc-900
-                    break-words">Com 18 anos e morando no Ceará, entrei de cabeça na programação frontend durante o ensino médio. Sou movido pela curiosidade, sempre em busca de desafios e apaixonado por tecnologia. Gosto de contribuir de forma proativa 
+                <section className="w-full flex justify-center items-center border-dashed border border-zinc-400 rounded-lg
+                md:flex-row md:gap-5 md:h-full
+                min-[320px]:flex-col min-[320px]:gap-0 min-[320px]:h-[40vh]">
+
+                    <div className="brightness-75 bg-[url(../assets/sidImage.jpeg)] bg-cover bg-center bg-no-repeat
+                    md:w-[200px] md:h-[200px] md:rounded-full md:outline md:outline-offset-4 md:outline-zinc-400 md:block
+                    min-[320px]:hidden"/>
+
+                    <p className="text-zinc-300 p-[20px] rounded-lg bg-zinc-900 break-words flex items-center
+                    md:w-[40vw] md:h-[30vh] md:py-5
+                    min-[450px]:text-[15px]
+                    min-[320px]:w-full min-[320px]:text-[14px] min-[320px]:h-full">Com 18 anos e morando no Ceará, entrei de cabeça na programação frontend durante o ensino médio. Sou movido pela curiosidade, sempre em busca de desafios e apaixonado por tecnologia. Gosto de contribuir de forma proativa 
                         em projetos inovadores. Se precisar de alguém cheio de energia e criatividade para seu projeto, tô pronto para embarcar!</p>
+
                 </section>
 
             </main>
--- src/components/siteComponents/iconsAboutPage.tsx
+++ src/components/siteComponents/iconsAboutPage.tsx
@@ -6,50 +6,54 @@ export function IconsAbout(){
     return(
         <>
             <ScrollArea>
-                <DivIcons>
-                    <SiTailwindcss className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Tailwindcss</p>
-                </DivIcons>
-                
-                <DivIcons>
-                    <FaReact className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">React</p>
-                </DivIcons>
+                <section className="flex
+                lg:flex-col lg:gap-8 lg:mt-0
+                min-[320px]:flex-row min-[320px]:gap-5 min-[320px]:flex-wrap min-[320px]:mt-[30px]">
+                        <DivIcons>
+                            <SiTailwindcss className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Tailwindcss</p>
+                        </DivIcons>
+                        
+                        <DivIcons>
+                            <FaReact className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">React</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiNextdotjs className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Next.js</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiNextdotjs className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Next.js</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiJavascript className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Javascript</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiJavascript className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Javascript</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiHtml5 className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Html5</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiHtml5 className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Html5</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiCss3 className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Css3</p>                                 
-                </DivIcons>
+                        <DivIcons>
+                            <SiCss3 className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Css3</p>                                 
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiBootstrap className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Bootstrap</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiBootstrap className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Bootstrap</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiGit className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">Git</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiGit className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">Git</p>
+                        </DivIcons>
 
-                <DivIcons>
-                    <SiNodedotjs className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
-                    <p className="text-[15px] text-zinc-400 cursor-default">NodeJs</p>
-                </DivIcons>
+                        <DivIcons>
+                            <SiNodedotjs className="text-[25px] text-zinc-200 hover:text-emerald-500 transition-colors duration-300"/>
+                            <p className="text-[15px] text-zinc-400 cursor-default">NodeJs</p>
+                        </DivIcons>
+                </section>
             </ScrollArea>
         </>
     )
@@ -62,7 +66,9 @@ type divElements = {
 const DivIcons = ({ children }: divElements) => {
     return(
         <>
-            <div className="h-[70px] flex gap-3 text-zinc-300]">
+            <div className="h-[30px] flex text-zinc-300
+            lg:gap-3
+            min-[320px]:gap-2">
                 {children}
             </div>
         </>
